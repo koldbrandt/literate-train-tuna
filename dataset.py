2c76fd2b895e0c94b9f6e5a3b05783fbc8d67dc7
Marcus <mailtilmb@gmail.com>
2022-06-03 13:52:06 +0200

    Added some stuff from the notebooks

---
+++ dataset.py
@@ -0,0 +1,55 @@
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+from tqdm.notebook import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+
+class Hotdog_NotHotdog(torch.utils.data.Dataset):
+    def __init__(self, train, transform, data_path='/dtu/datasets1/02514/hotdog_nothotdog'):
+        'Initialization'
+        self.transform = transform
+        data_path = os.path.join(data_path, 'train' if train else 'test')
+        image_classes = [os.path.split(d)[1] for d in glob.glob(data_path +'/*') if os.path.isdir(d)]
+        image_classes.sort()
+        self.name_to_label = {c: id for id, c in enumerate(image_classes)}
+        self.image_paths = glob.glob(data_path + '/*/*.jpg')
+        
+    def __len__(self):
+        'Returns the total number of samples'
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        'Generates one sample of data'
+        image_path = self.image_paths[idx]
+        
+        image = Image.open(image_path)
+        c = os.path.split(os.path.split(image_path)[0])[1]
+        y = self.name_to_label[c]
+        X = self.transform(image)
+        return X, y
+
+
+
+def get_data():
+    size = 128
+    train_transform = transforms.Compose([transforms.Resize((size, size)), 
+                                        transforms.ToTensor()])
+    test_transform = transforms.Compose([transforms.Resize((size, size)), 
+                                        transforms.ToTensor()])
+
+    batch_size = 64
+    trainset = Hotdog_NotHotdog(train=True, transform=train_transform)
+    train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
+    testset = Hotdog_NotHotdog(train=False, transform=test_transform)
+    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
+
+    return train_loader, test_loader
---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+from tqdm.notebook import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+import dataset
+
+def train(model, optimizer, num_epochs=10):
+    def loss_fun(output, target):
+        return F.nll_loss(output, target)
+    out_dict = {'train_acc': [],
+              'test_acc': [],
+              'train_loss': [],
+              'test_loss': []}
+  
+    for epoch in tqdm(range(num_epochs), unit='epoch'):
+        model.train()
+        #For each epoch
+        train_correct = 0
+        train_loss = []
+        for minibatch_no, (data, target) in tqdm(enumerate(train_loader), total=len(train_loader)):
+            data, target = data.to(device), target.to(device)
+            #Zero the gradients computed for each weight
+            optimizer.zero_grad()
+            #Forward pass your image through the network
+            output = model(data)
+            #Compute the loss
+            loss = loss_fun(output, target)
+            #Backward pass through the network
+            loss.backward()
+            #Update the weights
+            optimizer.step()
+
+            train_loss.append(loss.item())
+            #Compute how many were correctly classified
+            predicted = output.argmax(1)
+            train_correct += (target==predicted).sum().cpu().item()
+        #Comput the test accuracy
+        test_loss = []
+        test_correct = 0
+        model.eval()
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            with torch.no_grad():
+                output = model(data)
+            test_loss.append(loss_fun(output, target).cpu().item())
+            predicted = output.argmax(1)
+            test_correct += (target==predicted).sum().cpu().item()
+        out_dict['train_acc'].append(train_correct/len(trainset))
+        out_dict['test_acc'].append(test_correct/len(testset))
+        out_dict['train_loss'].append(np.mean(train_loss))
+        out_dict['test_loss'].append(np.mean(test_loss))
+        print(f"Loss train: {np.mean(train_loss):.3f}\t test: {np.mean(test_loss):.3f}\t",
+              f"Accuracy train: {out_dict['train_acc'][-1]*100:.1f}%\t test: {out_dict['test_acc'][-1]*100:.1f}%")
+    return out_dict
+
+
+
+def main()
+    train_data, test_data = dataset.get_data()
+    
+
+if __name__ == "__main__":
+    main()
+
