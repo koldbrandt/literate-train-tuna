5f462605226728e040cae791fc1c61995480ec8d
granatb <bartek.granat@gmail.com>
2022-06-10 14:49:44 +0200

    FIRST WORKING MODEL 1_2

--- project12/dataset.py
+++ project12/dataset.py
@@ -1,5 +1,6 @@
 #matplotlib inline
 import glob
+from math import floor
 import os
 
 import json
@@ -16,55 +17,48 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from tqdm.notebook import tqdm
+from torch import utils
+import cv2
+
+import random
+from math import floor
 
 class WasteInWild(torch.utils.data.Dataset):
-    def __init__(self, train, transform, data_path='dtu/datasets1/02514/data_wastedetection'):
+    def __init__(self, images, labels, transform):
         'Initialization'
         self.transform = transform
-        data_path = os.path.join(data_path, 'train' if train else 'test')
-        self.image_paths = glob.glob(data_path + '/*/*.jpg')
-        anns_file_path = data_path + '/' + 'annotations.json'
-
-        with open(anns_file_path, 'r') as f:
-            dataset = json.loads(f.read())
-
-        self.categories = dataset['categories']
-        self.anns = dataset['annotations']
-        self.imgs = dataset['images']
+        self.images = images
+        self.labels = labels
 
     def __len__(self):
         'Returns the total number of samples'
-        return len(self.image_paths)
+        return len(self.images)
 
-    def __getparams__(self):
-        categories = self.categories
-        anns = self.anns
-        imgs = self.imgs
-        nr_cats = len(categories)
-        nr_annotations = len(anns)
-        nr_images = len(imgs)
-        # Load categories and super categories
-        cat_names = []
-        super_cat_names = []
-        super_cat_ids = {}
-        super_cat_last_name = ''
-        nr_super_cats = 0
-        for cat_it in categories:
-            cat_names.append(cat_it['name'])
-            super_cat_name = cat_it['supercategory']
-            # Adding new supercat
-            if super_cat_name != super_cat_last_name:
-                super_cat_names.append(super_cat_name)
-                super_cat_ids[super_cat_name] = nr_super_cats
-                super_cat_last_name = super_cat_name
-                nr_super_cats += 1
-        return categories, anns, imgs, nr_cats, nr_super_cats, nr_annotations, nr_images, super_cat_names, cat_names, super_cat_ids 
-    
-def get_data(batch_size=16):
-    size = 256  
-    trainset = WasteInWild(train=True)
+    def __getitem__(self, idx):
+        image = self.images[idx]
+
+        X = self.transform(image)
+        y = self.labels[idx][0]
+        return  X, y  
+
+def get_data(train_images, train_labels, batch_size=16):
+    size = 32 
+
+
+    train_transform = transforms.Compose([
+                                        transforms.ToPILImage(),
+                                        transforms.Resize((size, size)), 
+                                        transforms.ToTensor(), 
+                                        ])
+
+    test_transform = transforms.Compose([transforms.Resize((size, size)), 
+                                        transforms.ToTensor()
+                                        ])
+
+    trainset = WasteInWild(train_images, train_labels, transform=train_transform)
     train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=3)
-    testset = WasteInWild(train=False)
-    test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=3)
 
-    return train_loader, test_loader
+    return train_loader
+
+
+
