6d392056fa51bac8bc36db59ab4233d47262b632
Marcus Koldborg Brandt <mailtilmb@gmail.com>
2022-06-10 14:58:48 +0200

    added nmf to utils

--- project11/utilities.py
+++ project11/utilities.py
@@ -39,3 +39,41 @@ def save_training_stats(stats, filename):
     # Save the training statistics to a csv
     stats_df = pd.DataFrame(stats)
     stats_df.to_csv("trainstats/"+filename)
+
+
+
+def nms(boxes, threshold):
+    """
+    Non-maximum suppression.
+    :param boxes: np.array of bounding boxes (x1, y1, x2, y2)
+    :param threshold: overlap threshold
+    :return: list of indices of bounding boxes to keep
+    """
+    x1 = boxes[:, 0]
+    y1 = boxes[:, 1]
+    x2 = boxes[:, 2]
+    y2 = boxes[:, 3]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = np.argsort(areas)
+
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(ovr <= threshold)[0]
+        order = order[inds + 1]
+
+    return keep
